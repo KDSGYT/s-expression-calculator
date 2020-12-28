04c54f03dd1eb9a19c8202b49c68c8f02a4d082a
Karan Pal Singh <kdsgyt@gmail.com>
2020-12-28 17:00:23 -0500

    updated .gitignore

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const arguments = require('process').argv.splice(2);// Fetched all the arguments
 const { add } = require('./functions/add')
 const { multiply } = require('./functions/multiply')
 const calCheck = require('./functions/functions');
+const mathjs = require('mathjs');
 // exports.main = () => {
 // for (let i = 0; i < arguments.length; i += 3) {
 // console.log(arguments.length)
@@ -10,10 +11,40 @@ const calCheck = require('./functions/functions');
 // calCheck(arguments[i], arguments[i + 1], arguments[i + 2]), arguments[i + 3]
 // 
 // )
-let finalArgument = arguments.filter(item => item!=')' );
+// let finalArgument = arguments.filter(item => item!=')' );
 // }
 // console.log(...finalArgument);
-console.log(
-    calCheck(...finalArgument)
 
+
+const expression = {}
+arguments.forEach((item, index) => {
+    if (item === ('add') || item === ('(add')) {
+        console.log(item)
+        if (arguments[index+1] === ('multiply') || arguments[index+1] === ('(multiply')) {
+            expression.add = [ {multiply: [arguments[index + 2], arguments[index + 3]]}, arguments[index + 4]]
+        }
+        else {
+            expression = {add:[arguments[index + 1], arguments[index + 2]]}
+
+        }
+    } else if (item === ('multiply') || item === ('(multiply')) {
+        expression.multiply = [arguments[index + 1], arguments[index + 2]]
+    }
+
+})
+
+
+
+/**
+ * To-Do
+ * 
+ * 1.Get multiple arguments 
+ * 2. Do multi level calculations
+ * 3. do calculations with chaining
+ * 
+ */
+console.log(
+    // calCheck(...finalArgument)
+    // finalArgument
+    expression.add
 )
